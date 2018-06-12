07c0fbf6298b78c9c3442606bf0287689700f7d1
tIoImIcIaItI <djkinggithub@outlook.com>
2018-06-12 14:07:21 -0600

    feat: Initial, callback-based wrapper for AsyncStorage

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Deck from './components/Deck';
 import AddDeck from './components/AddDeck';
 import AddCard from './components/AddCard';
 import Quiz from './components/Quiz';
+import DataStore from './data/data';
 
 // Allow users to create a deck which can hold an unlimited number of cards.
 // Allow users to add a card to a specific deck.
@@ -12,6 +13,8 @@ import Quiz from './components/Quiz';
 // The back of the card should display the answer.
 // Users should be able to quiz themselves on a specific deck and receive a score once they're done.
 // Users should receive a notification to remind themselves to study if they haven't already for that day.
+
+await DataStore.addSampleData();
 
 const App = createStackNavigator({
   Decks: { screen: Decks },
